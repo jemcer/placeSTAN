a1a5ced48e420d074600ce532b6340ed3a92a127
jemcer <i25@gmx.de>
2023-07-23 16:30:41 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,36 +1,39 @@
 // ==UserScript==
 // @name         r/placeDE Template - Kopie
 // @namespace    http://tampermonkey.net/
-// @version      8.3
+// @version      8.3.1
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/jemcer/placeSTAN/raw/main/script.user.js
+// @downloadURL  https://github.com/jemcer/placeSTAN/raw/main/script.user.js
 // ==/UserScript==
 
-var overlayImage = null;
-window.top !== window.self &&
+const overlayImage = document.createElement('img');
+overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1500px;pointerEvents: 'none';`;
+
+function updateImage() {
+  const img_url = `https://raw.githubusercontent.com/jemcer/placeSTAN/main/overlay_target.png`;
+  overlayImage.src = img_url + '?' + Date.now();
+}
+
+if (window.top !== window.self) {
   window.addEventListener(
     'load',
     () => {
-      let e = document
+      const canvasContainer = document
         .querySelector('garlic-bread-embed')
         .shadowRoot.querySelector('div.layout')
         .querySelector('garlic-bread-canvas')
         .shadowRoot.querySelector('div.container');
-      (overlayImage = document.createElement('img')),
-        updateImage(),
-        (overlayImage.style =
-          "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1500px;pointerEvents: 'none';"),
-        e.appendChild(overlayImage);
+
+      canvasContainer.appendChild(overlayImage);
+
+      updateImage();
     },
-    !1
+    false
   );
-const img_url =
-  'https://cdn.discordapp.com/attachments/831534229552627793/1132656928942723132/overlay_target.png';
-function updateImage() {
-  overlayImage.src = img_url;
 }
-setInterval(function () {
-  overlayImage.src = img_url;
-}, 3e4);
+
+setInterval(updateImage, 30000);
