7eac37eaee79e0e06bcd9e043d5084e9083d76fe
jemcer <i25@gmx.de>
2023-07-23 21:25:38 +0200

    goodbye github (bad cdn)

--- script.user.js
+++ script.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         r/placeDE Template - Kopie
 // @namespace    http://tampermonkey.net/
-// @version      8.4.4
+// @version      8.4.5
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/jemcer/placeSTAN/raw/main/script.user.js
-// @downloadURL  https://github.com/jemcer/placeSTAN/raw/main/script.user.js
+// @updateURL    https://test.heystan.de/pixel/script.user.js
+// @downloadURL  https://test.heystan.de/pixel/script.user.js
 // ==/UserScript==
 
 const overlayImage = document.createElement('img');
 overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;pointerEvents: 'none';`;
 
 function updateImage() {
-  const img_url = `https://raw.githubusercontent.com/jemcer/placeSTAN/main/overlay_target.png`;
+  const img_url = `https://test.heystan.de/pixel/overlay_target.png`;
   overlayImage.src = img_url + '?' + Date.now();
 }
 
